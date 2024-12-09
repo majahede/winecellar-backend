37d9e6661113d6c869e097670aa3407c0bace448
Maja Hedegärd <majahedegard@gmail.com>
2024-12-09 10:24:18 +0100

    add automapper

--- Winecellar.Application/Common/Mappings/MappingProfile.cs
+++ Winecellar.Application/Common/Mappings/MappingProfile.cs
@@ -1,11 +1,14 @@
+using AutoMapper;
+using Winecellar.Application.Dtos.Wines;
+using Winecellar.Domain.Models;
 
 namespace Winecellar.Application.Common.Mappings
 {
-    public class MappingProfile
+    public class MappingProfile : Profile
     {
         public MappingProfile()
         {
-            //CreateMap<Wine, WineDto>();
+            CreateMap<Wine, WineDto>();
         }
     }
 }
